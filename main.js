1af01e0dfcc6ea934a755d9034da19801d836120
jkes921003 <921003jun@gmail.com>
2024-12-13 12:04:23 +0800

    0

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 const app = Vue.createApp({
-    data() {
-      return {
-        navLinks: [
-          { text: "首頁", href: "rtfantasy.html" },
-          { text: "關於", href: "about.html" },
-          { text: "作品", href: "#services" },
-        ],
-      };
-    },
-  });
-  
-  app.mount("#app");
-  
+  data() {
+    return {
+      navLinks: [
+        { text: "首頁", href: "rtfantasy.html" },
+        { text: "關於", href: "about.html" },
+      ],
+      dropdownOptions: [
+        { text: "精選作品", href: "work.html" },
+        { text: "攝影", href: "photo.html" },
+        { text: "基礎設計", href: "design.html" },
+      ],
+    };
+  },
+});
+
+app.mount("#app");
