291f58d68700ce20d2f8fc418c6f8375487097fe
jkes921003 <921003jun@gmail.com>
2024-12-20 11:59:54 +0800

    0

--- main.js
+++ main.js
@@ -15,3 +15,58 @@ const app = Vue.createApp({
 });
 
 app.mount("#app");
+
+const  Carousel= Vue.createApp({
+  data() {
+    return {
+      // 定義幻燈片的資料
+      carouselItems: [
+        {
+          title: "", // 標題
+          image: "https://picsum.photos/2560/2560/?gravity=east", // 圖片來源
+        },
+        {
+          title: "", // 標題
+          image: "https://picsum.photos/2560/2560/?gravity=east", // 圖片來源
+        },
+        {
+          title: "", // 標題
+            image: "https://picsum.photos/2560/2560/?gravity=east", // 圖片來源
+        },
+      ],
+    };
+  },
+});
+
+
+Carousel.mount("#Carousel"); // 將 Vue 應用程式掛載到 HTML 的 #Carousel
+
+const container = Vue.createApp({
+  data() {
+    return {
+      // 定義卡片的資料
+      cards: [
+        {
+          title: "縮時攝影", // 卡片標題
+          text: "Some example text. John Doe is an architect and engineer.", // 卡片內容文字
+          image: "https://picsum.photos/200/200/?gravity=east", // 卡片圖片來源
+          link: "photo.html", // 按鈕連結
+        },
+        {
+          title: "偶動畫", // 卡片標題
+          text: "Explore the fascinating world of stop-motion animation.", // 卡片內容文字
+          image: "https://picsum.photos/200/200/?gravity=west", // 卡片圖片來源
+          link: "design.html", // 按鈕連結
+        },
+        {
+          title: "光柵動畫", // 卡片標題
+          text: "Learn about raster animation techniques and creativity.", // 卡片內容文字
+          image: "https://picsum.photos/200/200/?gravity=north", // 卡片圖片來源
+          link: "design.html", // 按鈕連結
+        },
+      ],
+    };
+  },
+});
+
+container.mount("#container");
