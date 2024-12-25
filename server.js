9e8b85413755281940ef3167174458094ab52a37
jkes921003 <921003jun@gmail.com>
2024-12-25 17:51:06 +0800

    0

--- server.js
+++ server.js
@@ -2,12 +2,10 @@
 //install web server package: express >npm install express
 var express = require("express");
 var server = express();
-var bodyParser = require("body-parser");
+
 
 //web root
 server.use(express.static(__dirname));
-server.use(bodyParser.json());
-server.use(bodyParser.urlencoded());
 
 var fileUpload = require("express-fileupload");
 server.use(fileUpload({defCharset:'utf8', defParamCharset:'utf8'}));
